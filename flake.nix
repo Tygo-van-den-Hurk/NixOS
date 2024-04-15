bfd56d01695dccd5175bbf99034b230537eda66a
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-04-15 10:30:17 +0200

    Worked on getting the flake working.

--- flake.nix
+++ flake.nix
@@ -3,16 +3,44 @@
   
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ INPUTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-    inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    inputs = {
+        nixpkgs.url = github:nixos/nixpkgs?ref=nixos-unstable;
+        home-manager = {
+            url = github:nix-community/home-manager;
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ OUTPUTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-    outputs = { self, nixpkgs, ... } : let
+    outputs = { self, nixpkgs, home-manager, ... } : let
         lib = nixpkgs.lib;
        pkgs = import nixpkgs;
-    in { nixosConfigurations = {
-           tygos-thinkpad = lib.nixosSystem { modules = [ ./systems/laptops/thinkpad/configuration.nix ]; };
-            tygos-desktop = lib.nixosSystem { modules = [ ./systems/desktops/home/configuration.nix ]; };
-        };        
+    in { nixosConfigurations = { #` Systems
+            
+            tygos-desktop = lib.nixosSystem { 
+                modules = [ 
+                    ./systems/desktops/home/configuration.nix 
+                ];
+            };
+
+           tygos-thinkpad = lib.nixosSystem { 
+                modules = [ 
+                    ./systems/laptops/thinkpad/configuration.nix 
+                ]; 
+            };
+
+            default = { # TODO : make this throw an error when loaded as this is not a system
+                # assert false || throw "Did not specify hostname. 
+                #     Please run the command ending in `.#hostname` 
+                #     where hostname is the hostname of the system 
+                #     you would like to load.";
+            };
+        };
+
+        #` Home manager
+        hmConfig = {
+
+        };     
     };
 }
