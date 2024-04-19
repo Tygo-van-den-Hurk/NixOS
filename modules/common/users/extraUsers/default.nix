5a06bf4755be93947ac6e498ea2b887f2fc2d3bc
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-04-19 11:09:09 +0200

    Fixed an output error in '/modules/common/users/extraUsers/default.nix', where it said '/modules/common/users.extraUsers...', instead of '/modules/common/users/extraUsers...'.

--- modules/common/users/extraUsers/default.nix
+++ modules/common/users/extraUsers/default.nix
@@ -4,7 +4,7 @@ arguments @ { config, pkgs, lib, machine-settings, ... } : let
     #! make sure that the variable that `builtins.trace` assigns get used to trigger the print.
     #` this is because `builtins.trace` only prints a trace on the output if the variable gets used.
     #` that's why you have to go through hoops and bounds to get this variable used so that it prints the message.
-    initial-password = builtins.trace ("Loading: /modules/common/users.extraUsers...") ("changeme"); 
+    initial-password = builtins.trace ("Loading: /modules/common/users/extraUsers...") ("changeme"); 
 
 in { users.extraUsers = {
 
