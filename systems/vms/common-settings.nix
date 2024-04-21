c09e96e727a1a0ab590a949aae851320f15d1b19
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-04-21 15:32:50 +0200

    Delete systems/vms directory
    Until another system is being tracked this just added extra things to keep into account.

--- systems/vms/common-settings.nix
+++
@@ -1,7 +0,0 @@
-## This file contains the common settings for this category.
-#! This file will overwrite the global settings!
-let common-settings = import ../common-settings.nix // {
-
-    # add overrides here...
-
-}; in common-settings
