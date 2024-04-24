6c9d9462e821d26429c2e7067d8ae5d701664645
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-04-24 13:39:24 +0200

    Removes some old services out of the common module.

--- modules/common/services/default.nix
+++ modules/common/services/default.nix
@@ -2,15 +2,15 @@
 
 arguments @ { config, pkgs, lib, machine-settings, ... } : let 
     
+    #! make sure that the variable that `builtins.trace` assigns get used to trigger the print.
+    #` this is because `builtins.trace` only prints a trace on the output if the variable gets used.
+    #` that's why you have to go through hoops and bounds to get this variable used so that it prints the message.
     arguments_ = builtins.trace "Loading: /modules/common/services..." arguments; 
 
-in { services = { };
-
-    imports = [ 
+in { imports = [ 
         ( import ./openssh   arguments_ )
         ( import ./pipewire  arguments_ )
         ( import ./printing  arguments_ )
         ( import ./tailscale arguments_ )
-        #// ( import ./xserver   arguments )
     ]; 
 }
