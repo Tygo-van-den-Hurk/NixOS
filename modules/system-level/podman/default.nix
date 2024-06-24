822c2ca20b9e7941a155800462aa8e6f42792551
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-06-24 18:54:55 +0200

    Added the 'podman' system level module.

---
+++ modules/system-level/podman/default.nix
@@ -0,0 +1,26 @@
+## Enabled podman as a service.
+
+arguments @ { config, pkgs, lib, machine-settings, programs, input, ... } : let 
+    
+    #! make sure that the variable that `builtins.trace` assigns get used to trigger the print.
+    #` this is because `builtins.trace` only prints a trace on the output if the variable gets used.
+    #` that's why you have to go through hoops and bounds to get this variable used so that it prints the message.
+    podman-module-settings = (builtins.trace "Loading: ${toString ./}..." machine-settings.system.modules.podman ); 
+
+in ( if  then { 
+        
+    virtualisation = {
+        containers.enable = true;
+        podman = {
+            enable = true;
+            dockerCompat = podman-module-settings.dockerCompat;
+            defaultNetwork.settings.dns_enabled = true;
+        };
+    };
+
+    environment.systemPackages = with pkgs; [
+        dive
+        podman-tui
+        podman-compose
+    ];
+})
