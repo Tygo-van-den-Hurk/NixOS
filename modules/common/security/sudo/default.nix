1f77b6727d50dd3ca1e3457ffc90d741739e9d0b
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-12-21 11:16:36 +0100

    Disabled sudo without a password and added the asterics when typing instead.

--- modules/common/security/sudo/default.nix
+++ modules/common/security/sudo/default.nix
@@ -1,9 +1,9 @@
 ## Defines display manager / window manager settings.
 
 arguments @ { config, pkgs, lib, machine-settings, ... } : ( builtins.trace "(System) Loading: ${toString ./.}..." { 
-    
-    security.sudo = {
-        wheelNeedsPassword = lib.mkDefault false;
-    };
+  security.sudo = {
+    wheelNeedsPassword = lib.mkDefault true;
+    extraConfig = "Defaults pwfeedback";
+  };
 })
 
