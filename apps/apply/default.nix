c714fd472bab28706efa802789e11ad0cd7a286f
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-02-10 16:35:15 +0100

    chore: remove apply as a package
    I dont need CI to check if it builds as it is a shell script. Meaning all
    errors will be run time anyways.

--- apps/apply/default.nix
+++ apps/apply/default.nix
@@ -6,45 +6,42 @@ _: {
       lib,
       ...
     }:
+    with pkgs;
     let
       name = "apply";
-      runtimeDependencies = with pkgs; [ ];
+      package = stdenv.mkDerivation rec {
+        inherit name;
+        src = ./.;
+
+        nativeBuildInputs = runtimeDependencies ++ [ makeWrapper ];
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir --parents $out/share/bash-completion/completions
+          cp ${src}/completions.bash $out/share/bash-completion/completions/${name}.bash
+
+          mkdir --parents $out/bin
+          cp ${src}/script.bash $out/bin/${name}
+          patchShebangs $out/bin/${name}
+          wrapProgram $out/bin/${name} --prefix PATH : ${lib.makeBinPath runtimeDependencies}
+
+          runHook postInstall
+        '';
+
+        meta = with lib; {
+          description = "Conveniently build and switch to your new configuration.";
+          homepage = "https://github.com/Tygo-van-den-Hurk/NixOS";
+          license = with licenses; [ epl20 ];
+          maintainers = with maintainers; [ Tygo-van-den-Hurk ];
+          mainProgram = name;
+        };
+      };
     in
     {
-      packages.${name} =
-        with pkgs;
-        stdenv.mkDerivation rec {
-          inherit name;
-          src = ./.;
-
-          nativeBuildInputs = runtimeDependencies ++ [ makeWrapper ];
-
-          installPhase = ''
-            runHook preInstall
-
-            mkdir --parents $out/share/bash-completion/completions
-            cp ${src}/completions.bash $out/share/bash-completion/completions/${name}.bash
-
-            mkdir --parents $out/bin
-            cp ${src}/script.bash $out/bin/${name}
-            patchShebangs $out/bin/${name}
-            wrapProgram $out/bin/${name} --prefix PATH : ${lib.makeBinPath runtimeDependencies}
-
-            runHook postInstall
-          '';
-
-          meta = with lib; {
-            description = "Conveniently build and switch to your new configuration.";
-            homepage = "https://github.com/Tygo-van-den-Hurk/NixOS";
-            license = with licenses; [ epl20 ];
-            maintainers = with maintainers; [ Tygo-van-den-Hurk ];
-            mainProgram = name;
-          };
-        };
-
       apps.${name} = {
-        program = lib.getExe self'.packages.${name};
-        inherit (self'.packages.${name}) meta;
+        program = lib.getExe package;
+        inherit (package) meta;
       };
     };
 }
