676bc4694b0565e3ff6fcfff415ca008af81d2be
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-02-18 17:28:07 +0100

    feat: add btop config
    This config add the looks of btop to stylix.

---
+++ modules/home/cli/miscellaneous/btop.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+with lib;
+let
+  type = "cli";
+  category = "miscellaneous";
+  program = "btop";
+  cfg = config.${type}.${category}.${program};
+in
+{
+  options.${type}.${category}.${program} = with types; {
+    enable = mkOption {
+      description = "Whether to enable ${program}'s default config.";
+      default = config.${type}.${category}.enable;
+      type = bool;
+    };
+  };
+
+  config.programs.${program} = mkIf cfg.enable {
+    enable = mkDefault true;
+    settings = {
+      proc_tree = mkDefault false;
+    };
+  };
+}
