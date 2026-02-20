10ec056dbc23363b38ac22655ff810e4ec37707e
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-02-20 21:37:05 +0100

    chore: symlink current compatibility file to target
    The file `$REPO/homes/tygo@tygos-thinkpad-nixos/home.nix` is now a symbolic
    link to `$REPO/hosts/thinkpad/home.nix`. So they will always match each other.

--- hosts/thinkpad/home.nix
+++ hosts/thinkpad/home.nix
@@ -1,11 +1,55 @@
 {
-  lib,
+  inputs,
+  system,
   ...
 }:
-with lib;
+let
+  inherit (inputs.tygo-van-den-hurk-dotfiles.packages.${system}) preview;
+in
 {
   cli.enable = true;
-  gui.enable = false;
+  cli.miscellaneous.zellij.enable = false;
+  gui.enable = true;
   styling.enable = true;
-  home.stateVersion = "25.11";
+  wm.enable = true;
+
+  home.username = "tygo";
+  home.homeDirectory = "/home/tygo";
+  home.stateVersion = "25.05";
+  home.packages = [
+    preview
+  ];
+
+  wm.monitors."builtin display" = {
+    primary = true;
+    adapter = "eDP-1";
+    refresh-rate = 60;
+    resolution.vertical = 1080;
+    resolution.horizontal = 1920;
+    position.vertical = 0;
+    position.horizontal = 0;
+  };
+
+  wm.monitors."HDMI port" = {
+    primary = true;
+    adapter = "HDMI-1";
+    refresh-rate = 60;
+    resolution.vertical = 1080 * 2;
+    resolution.horizontal = 1920 * 2;
+    position.vertical = 1080;
+    position.horizontal = 0;
+  };
+
+  wm.monitors."USB-C" = {
+    primary = true;
+    adapter = "DP-1";
+    refresh-rate = 60;
+    resolution.vertical = 1080 * 2;
+    resolution.horizontal = 1920 * 2;
+  };
+
+  # TEMP:
+
+  gui.messengers.thunderbird.enable = false;
+  programs.rofi.enable = true;
 }
