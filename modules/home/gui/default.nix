992aa57d6610f9667dbdab5f1c847d34cc642a7e
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-02-20 15:06:56 +0100

    fix: add alacritty scaling with DPI instead of what I pixels
    And enabled `xdg.mimeApps` as well.

--- modules/home/gui/default.nix
+++ modules/home/gui/default.nix
@@ -18,6 +18,10 @@ in
         };
       };
 
+      # Bug fixes: https://github.com/alacritty/alacritty/issues/5101;
+      config.home.sessionVariables.WINIT_X11_SCALE_FACTOR = 1;
+      config.xdg.mimeApps.enable = true;
+
       imports = inputs.self.lib.import-recursively {
         base = ./.;
         exclude = ./default.nix;
