5a7afb9c0f8b65dd947d50b5b31aa5ac147b96ef
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-02-25 22:26:38 +0100

    chore: update old base config files with new installation files
    The old `configuration.nix` and `hardware-configuration.nix` were based on
    my old drive and instance, which obviously isn't the same, thus I updated it
    with the files generated on installation.

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -8,8 +8,22 @@ with lib;
     ./hardware-configuration.nix
   ];
 
-  self.all.enable = true;
-  self.impermanence.bootDeviceID = "3AF8-8F1E";
+  self.defaults.enable = true;
+  self.gaming.enable = true;
+  self.nas.enable = true;
+  self.qmk.enable = true;
+  self.ssh.enable = true;
+  self.gui.enable = true;
+
+  # Extra code that was present at the generation of the config.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
   #! This value determines the NixOS release from which the default
   #! settings for stateful data, like file locations and database versions
@@ -17,7 +31,7 @@ with lib;
   #! this value at the release version of the first install of this system.
   #! Before changing this value read the documentation for this option
   #! (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = mkForce "23.11"; # ? Did you read the comment?
+  system.stateVersion = "25.11"; # Did you read the comment?
 
   boot.loader.grub.devices = [ "nodev" ];
 }
