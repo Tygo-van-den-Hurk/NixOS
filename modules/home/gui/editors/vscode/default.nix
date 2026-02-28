1e057b86c1be5456a1b5a3073d0e526b0b95f113
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-02-28 12:52:31 +0100

    feat: add `vscode` package to the home manger environment
    This is the main IDE I use right now. I do have plans to move to VIM, but that
    is for the future.

---
+++ modules/home/gui/editors/vscode/default.nix
@@ -0,0 +1,52 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+with lib;
+let
+  namespace = "self";
+  type = "gui";
+  category = "editors";
+  program = "vscode";
+  cfg = config.${namespace}.${type}.${category}.${program};
+in
+{
+  options.${namespace}.${type}.${category}.${program} = with types; {
+
+    enable = mkOption {
+      description = "Whether to enable ${program}'s default config.";
+      default = config.${namespace}.${type}.${category}.enable;
+      type = bool;
+    };
+
+    package = mkOption {
+      description = "The package to use for ${program}.";
+      default = pkgs.vscodium;
+      type = package;
+    };
+
+    packageName = mkOption {
+      description = "The package to use for ${program}.";
+      default =
+        cfg.package.meta.mainProgram or (throw "No main program provided in the meta of this package.");
+      type = str;
+    };
+
+    mkDefault = mkOption {
+      description = "Whether to make ${program} the default program to open its file type.";
+      default = cfg.enable;
+      type = bool;
+    };
+  };
+
+  config.programs.${program} = mkIf cfg.enable {
+    inherit (cfg) enable;
+    inherit (cfg) package;
+  };
+
+  config.home = mkIf cfg.mkDefault rec {
+    shellAliases.code = cfg.packageName;
+  };
+}
