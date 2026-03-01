a602a0fcafb3813d89986a5c5e8d91cbaf469e8e
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-03-01 17:38:39 +0100

    feat: add snowflake shortcut support
    Now keybinds include Snowflake key binds. Snowflake is my custom keyboard,
    which can handle complex keyboard events. Sending HYPR keybinds for special
    events like: `CTRL+ALT+SHIFT+SUPER+1`. I wanna use those to let snowflake
    switch to a certain workspace.

--- modules/home/wm/shortcuts/focus-and-workspaces.nix
+++ modules/home/wm/shortcuts/focus-and-workspaces.nix
@@ -43,6 +43,55 @@ in
         }) amountOfWorkspaces
       );
 
+      moveFocusToWorkspaceUsingSnowflake = listToAttrs (
+        genList (i: {
+          name = "moving focus to workspace ${toString (i + 1)} using snowflake";
+          value = {
+            control = true;
+            option = true;
+            super = true;
+            shift = true;
+            key = toString (i + 1);
+            action.hyprland = "workspace, ${toString (i + 1)}";
+            action.i3 = "workspace $ws${toString (i + 1)}";
+          };
+        }) amountOfWorkspaces
+      );
+
+      chars."1" = "a";
+      chars."2" = "b";
+      chars."3" = "c";
+      chars."4" = "d";
+      chars."5" = "e";
+      chars."6" = "f";
+      chars."7" = "g";
+      chars."8" = "h";
+      chars."9" = "i";
+      chars."0" = "j";
+
+      moveWindowToWorkspaceUsingSnowflake = listToAttrs (
+        genList (i: {
+          name = "moving window to workspace ${toString (i + 1)} using snowflake";
+          value = {
+            control = true;
+            option = true;
+            super = true;
+            shift = true;
+            key = chars.${toString (i + 1)};
+            action.hyprland = "moveToWorkspace, ${toString (i + 1)}";
+            action.i3 = "move container to workspace number $ws${toString (i + 1)}; workspace $ws${toString (i + 1)}";
+          };
+        }) amountOfWorkspaces
+      );
+
+      result = moveWindowToWorkspace
+        // moveFocusToWorkspace
+        // moveFocusToWorkspaceUsingSnowflake
+        // moveWindowToWorkspaceUsingSnowflake;
+
     in
-    moveWindowToWorkspace // moveFocusToWorkspace;
+      if amountOfWorkspaces > 10 then
+        throw "Snowflake can only handle up to 10 workspaces."
+      else 
+        result;
 }
