84f72f5d0d0dc335314b96d37c5b0d063b8545a3
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-03-05 08:38:19 +0100

    feat: add screenshot capabilities to hyprland setup
    I'm using `grimblast` as a backend for these screenshots.

--- modules/home/wm/shortcuts/screen-shots.nix
+++ modules/home/wm/shortcuts/screen-shots.nix
@@ -14,19 +14,19 @@ in
     #
     #  Screen shot keyboard shortcuts follow the MacOS key bindings:
     #
-    #  - tertiary modifier + shift + 3: full screen on your desktop
-    #  - tertiary modifier + shift + 4: selection on your desktop
-    #  - tertiary modifier + shift + 5: options on your desktop
+    #  - primary modifier + shift + 3: full screen on your desktop
+    #  - primary modifier + shift + 4: selection on your desktop
+    #  - primary modifier + shift + 5: options on your desktop
     #
     #  Add the `secondary modifier` for going to clipboard:
     #
-    #  - tertiary modifier + secondary modifier + shift + 3: putting full screen on your clipboard
-    #  - tertiary modifier + secondary modifier + shift + 4: putting selection on your clipboard
-    #  - tertiary modifier + secondary modifier + shift + 5: putting options on your clipboard
+    #  - primary modifier + secondary modifier + shift + 3: putting full screen on your clipboard
+    #  - primary modifier + secondary modifier + shift + 4: putting selection on your clipboard
+    #  - primary modifier + secondary modifier + shift + 5: putting options on your clipboard
     #
 
     # screen shots putting it on the desktop
-    "screenshot-entire-screen" = {
+    "screenshot-entire-screen" = rec {
       key = "3";
       shift = true;
       control = true;
@@ -35,8 +35,9 @@ in
         getExe (
           writeShellScriptBin "screenshot-entire-screen-hyprland" /* SHELL */ ''
             exec > >(systemd-cat -t screenshot-entire-screen-hyprland) 2>&1
-            echo "ERROR: no such program installed yet.."
-            exit 1 # TODO: fill in for hyprland
+            ${busybox}/bin/mkdir -p "''${HOME:-/home/$USER}/Desktop" && set -e
+            ${getExe grimblast} save output "''${HOME:-/home/$USER}/Desktop/$(date +%Y-%m-%d_%H-%M-%S).png"
+            ${getExe' mpg123 "mpg123"} -k 27 '${./screen-shot.mp3}' -v
           ''
         )
       }";
@@ -45,14 +46,15 @@ in
         getExe (
           writeShellScriptBin "screenshot-entire-screen-i3" /* SHELL */ ''
             exec > >(systemd-cat -t screenshot-entire-screen-i3) 2>&1
-            ${getExe flameshot} screen --path '~/Desktop/$(date +%Y-%m-%d_%H-%M-%S).png'
-            ${getExe' mpg123 "mpg123"} -k 27 ${./screen-shot.mp3} -v
+            ${busybox}/bin/mkdir -p "''${HOME:-/home/$USER}/Desktop" && set -e
+            ${getExe flameshot} screen --path "''${HOME:-/home/$USER}/Desktop/$(date +%Y-%m-%d_%H-%M-%S).png"
+            ${getExe' mpg123 "mpg123"} -k 27 '${./screen-shot.mp3}' -v
           ''
         )
       }";
     };
 
-    "screenshot-select-section" = {
+    "screenshot-select-section" = rec {
       key = "4";
       shift = true;
       control = true;
@@ -61,8 +63,9 @@ in
         getExe (
           writeShellScriptBin "screenshot-select-section-hyprland" /* SHELL */ ''
             exec > >(systemd-cat -t screenshot-select-section-hyprland) 2>&1
-            echo "ERROR: no such program installed yet.."
-            exit 1 # TODO: fill in for hyprland
+            ${busybox}/bin/mkdir -p "''${HOME:-/home/$USER}/Desktop" && set -e
+            ${getExe grimblast} save area "''${HOME:-/home/$USER}/Desktop/$(date +%Y-%m-%d_%H-%M-%S).png"
+            ${getExe' mpg123 "mpg123"} -k 27 '${./screen-shot.mp3}' -v
           ''
         )
       }";
@@ -71,14 +74,15 @@ in
         getExe (
           writeShellScriptBin "screenshot-select-section-i3" /* SHELL */ ''
             exec > >(systemd-cat -t screenshot-select-section-i3) 2>&1
-            ${getExe flameshot} gui --accept-on-select --path '~/Desktop/$(date +%Y-%m-%d_%H-%M-%S).png'
-            ${getExe' mpg123 "mpg123"} -k 27 ${./screen-shot.mp3} -v
+            ${busybox}/bin/mkdir -p "''${HOME:-/home/$USER}/Desktop" && set -e
+            ${getExe flameshot} gui --accept-on-select --path "''${HOME:-/home/$USER}/Desktop/$(date +%Y-%m-%d_%H-%M-%S).png"
+            ${getExe' mpg123 "mpg123"} -k 27 '${./screen-shot.mp3}' -v
           ''
         )
       }";
     };
 
-    "screenshot-special" = {
+    "screenshot-special" = rec {
       key = "5";
       shift = true;
       control = true;
@@ -97,8 +101,9 @@ in
         getExe (
           writeShellScriptBin "screenshot-special-i3" /* SHELL */ ''
             exec > >(systemd-cat -t screenshot-special-i3) 2>&1
-            ${getExe flameshot} gui --path '~/Desktop/$(date +%Y-%m-%d_%H-%M-%S).png'
-            ${getExe' mpg123 "mpg123"} -k 27 ${./screen-shot.mp3} -v
+            ${busybox}/bin/mkdir -p "''${HOME:-/home/$USER}/Desktop" && set -e
+            ${getExe flameshot} gui --path "''${HOME:-/home/$USER}/Desktop/$(date +%Y-%m-%d_%H-%M-%S).png"
+            ${getExe' mpg123 "mpg123"} -k 27 '${./screen-shot.mp3}' -v
           ''
         )
       }";
@@ -106,7 +111,7 @@ in
 
     # screen shots putting it on your clipboard
 
-    "screenshot-entire-screen-to-clipboard" = {
+    "screenshot-entire-screen-to-clipboard" = rec {
       key = "3";
       shift = true;
       control = true;
@@ -116,8 +121,9 @@ in
         getExe (
           writeShellScriptBin "screenshot-entire-screen-to-clipboard-hyprland" /* SHELL */ ''
             exec > >(systemd-cat -t screenshot-entire-screen-to-clipboard-hyprland) 2>&1
-            echo "ERROR: no such program installed yet.."
-            exit 1 # TODO: fill in for hyprland
+            ${busybox}/bin/mkdir -p "''${HOME:-/home/$USER}/Desktop" && set -e
+            ${getExe grimblast} copy output
+            ${getExe' mpg123 "mpg123"} -k 27 '${./screen-shot.mp3}' -v
           ''
         )
       }";
@@ -126,14 +132,15 @@ in
         getExe (
           writeShellScriptBin "screenshot-entire-screen-to-clipboard-i3" /* SHELL */ ''
             exec > >(systemd-cat -t screenshot-entire-screen-to-clipboard-i3) 2>&1
+            ${busybox}/bin/mkdir -p "''${HOME:-/home/$USER}/Desktop" && set -e
             ${getExe flameshot} screen --raw | xclip -selection clipboard -t image/png
-            ${getExe' mpg123 "mpg123"} -k 27 ${./screen-shot.mp3} -v
+            ${getExe' mpg123 "mpg123"} -k 27 '${./screen-shot.mp3}' -v
           ''
         )
       }";
     };
 
-    "screenshot-select-section-to-clipboard" = {
+    "screenshot-select-section-to-clipboard" = rec {
       key = "4";
       shift = true;
       control = true;
@@ -143,8 +150,9 @@ in
         getExe (
           writeShellScriptBin "screenshot-select-section-to-clipboard-hyprland" /* SHELL */ ''
             exec > >(systemd-cat -t screenshot-select-section-to-clipboard-hyprland) 2>&1
-            echo "ERROR: no such program installed yet.."
-            exit 1 # TODO: fill in for hyprland
+            ${busybox}/bin/mkdir -p "''${HOME:-/home/$USER}/Desktop" && set -e
+            ${getExe grimblast} copy area
+            ${getExe' mpg123 "mpg123"} -k 27 '${./screen-shot.mp3}' -v
           ''
         )
       }";
@@ -153,14 +161,15 @@ in
         getExe (
           writeShellScriptBin "screenshot-select-section-to-clipboard-i3" /* SHELL */ ''
             exec > >(systemd-cat -t screenshot-select-section-to-clipboard-i3) 2>&1
+            ${busybox}/bin/mkdir -p "''${HOME:-/home/$USER}/Desktop" && set -e
             ${getExe flameshot} gui --raw --accept-on-select | xclip -selection clipboard -t image/png
-            ${getExe' mpg123 "mpg123"} -k 27 ${./screen-shot.mp3} -v
+            ${getExe' mpg123 "mpg123"} -k 27 '${./screen-shot.mp3}' -v
           ''
         )
       }";
     };
 
-    "screenshot-special-to-clipboard" = {
+    "screenshot-special-to-clipboard" = rec {
       key = "5";
       shift = true;
       control = true;
@@ -180,8 +189,9 @@ in
         getExe (
           writeShellScriptBin "screenshot-special-to-clipboard-i3" /* SHELL */ ''
             exec > >(systemd-cat -t screenshot-special-to-clipboard-i3) 2>&1
+            ${busybox}/bin/mkdir -p "''${HOME:-/home/$USER}/Desktop" && set -e
             ${getExe flameshot} gui --raw | xclip -selection clipboard -t image/png
-            ${getExe' mpg123 "mpg123"} -k 27 ${./screen-shot.mp3} -v
+            ${getExe' mpg123 "mpg123"} -k 27 '${./screen-shot.mp3}' -v
           ''
         )
       }";
