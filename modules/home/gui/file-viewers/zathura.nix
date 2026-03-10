0135b8636ddad79b89d414df5980105e74dceb7d
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-03-10 16:12:24 +0100

    fix: nohup + zatura path not being a path to a executable
    I wrote `nohup ${pkgs.zathura}` instead of `nohup ${getExe pkgs.zathura}`,
    which is wrong. That does not exec anything.

--- modules/home/gui/file-viewers/zathura.nix
+++ modules/home/gui/file-viewers/zathura.nix
@@ -37,7 +37,7 @@ in
   config.programs.bash = mkIf cfg.enable {
     initExtra = ''
       function zathura() {
-        nohup ${config.programs.zathura.package} "$@" &> /dev/null &
+        nohup ${getExe config.programs.zathura.package} "$@" &> /dev/null &
       }
     '';
   };
@@ -45,7 +45,7 @@ in
   config.programs.zsh = mkIf cfg.enable {
     initExtra = ''
       zathura() {
-        nohup ${config.programs.zathura.package} "$@" &> /dev/null &
+        nohup ${getExe config.programs.zathura.package} "$@" &> /dev/null &
       }
     '';
   };
@@ -53,7 +53,7 @@ in
   config.programs.fish = mkIf cfg.enable {
     interactiveShellInit = ''
       function zathura
-        nohup ${config.programs.zathura.package} $argv > /dev/null 2>&1 &
+        nohup ${getExe config.programs.zathura.package} $argv > /dev/null 2>&1 &
       end
     '';
   };
