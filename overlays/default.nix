b05e7356198ae03c91a632ffb93543e586eee350
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-04-08 17:17:11 +0200

    fix: overlays causing infinite recursion
    I did not know how to solve this for so long, however the fix seems to use
    the `system` variable from STDENV, which seems to work.

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,18 @@
 { inputs, ... }:
 {
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will be accessible through 'pkgs.unstable'.
-  flake.overlays.unstable-packages = final: _prev: {
+  flake.overlays.unstable-packages = _: previous: {
     unstable = import inputs.nixpkgs-unstable {
-      inherit (final) system;
-      # inherit (final) config;
+      inherit (previous.stdenv) system;
       overlays = [ ];
     };
   };
+
+  # When applied, adds the packages of this flake to the package set.
+  flake.overlays.packages =
+    _: previous:
+    let
+      inherit (previous.stdenv) system;
+    in
+    inputs.self.packages.${system};
 }
