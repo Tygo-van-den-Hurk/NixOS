42d15bba18eba84fb9254a6c163067de6c2bfdac
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-04-19 16:53:05 +0200

    feat: add python and jupyter notebook support to VS Code
    I need support for this because of a course I am taking.

--- modules/home/gui/editors/vscode/profiles/default/extensions.nix
+++ modules/home/gui/editors/vscode/profiles/default/extensions.nix
@@ -16,35 +16,46 @@ in
 {
   config.programs.${program}.profiles.${profile} = mkIf cfg.enable {
     enableExtensionUpdateCheck = mkDefault false;
-    extensions = with pkgs.vscode-extensions; [
-      streetsidesoftware.code-spell-checker
-      catppuccin.catppuccin-vsc-icons
-      formulahendry.auto-rename-tag
-      github.vscode-github-actions
-      bradlc.vscode-tailwindcss
-      editorconfig.editorconfig
-      yoavbls.pretty-ts-errors
-      astro-build.astro-vscode
-      tamasfe.even-better-toml
-      myriad-dreamin.tinymist
-      mechatroner.rainbow-csv
-      oderwat.indent-rainbow
-      dbaeumer.vscode-eslint
-      ritwickdey.liveserver
-      usernamehw.errorlens
-      # dotenv.dotenv-vscode # wants to write to `settings.json` so gives errors as nix is read only
-      wmaurer.change-case
-      jnoortheen.nix-ide
-      bierner.color-info
-      # mhutchie.git-graph # Unfree :/
-      dotjoshjohnson.xml
-      eamodio.gitlens
-      tomoki1207.pdf
-      docker.docker
-      antfu.slidev
-      vue.volar
-      golang.go
-      jock.svg
-    ];
+    extensions =
+      with pkgs.vscode-extensions;
+      [
+        streetsidesoftware.code-spell-checker
+        catppuccin.catppuccin-vsc-icons
+        formulahendry.auto-rename-tag
+        github.vscode-github-actions
+        bradlc.vscode-tailwindcss
+        editorconfig.editorconfig
+        yoavbls.pretty-ts-errors
+        astro-build.astro-vscode
+        tamasfe.even-better-toml
+        myriad-dreamin.tinymist
+        mechatroner.rainbow-csv
+        oderwat.indent-rainbow
+        dbaeumer.vscode-eslint
+        ritwickdey.liveserver
+        usernamehw.errorlens
+        # dotenv.dotenv-vscode # wants to write to `settings.json` so gives errors as nix is read only
+        wmaurer.change-case
+        jnoortheen.nix-ide
+        bierner.color-info
+        # mhutchie.git-graph # Unfree :/
+        dotjoshjohnson.xml
+        haskell.haskell
+        eamodio.gitlens
+        tomoki1207.pdf
+        docker.docker
+        antfu.slidev
+        vue.volar
+        golang.go
+        jock.svg
+      ]
+      ++ [
+        # Python and Jupyter notebooks
+        ms-toolsai.jupyter-renderers
+        ms-toolsai.jupyter
+        ms-python.debugpy
+        ms-python.pylint
+        ms-python.python
+      ];
   };
 }
