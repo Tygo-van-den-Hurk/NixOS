3c360101a75206ffa80a10913ac6673170031afd
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-08-19 13:45:47 +0200

    feat: use new enlarged vscode extension collection
    This adds roughly 79.500 extensions that we can now also use.

--- modules/home/gui/editors/vscodium/profiles/default/extensions.nix
+++ modules/home/gui/editors/vscodium/profiles/default/extensions.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   config,
   pkgs,
   lib,
@@ -14,48 +15,48 @@ let
   cfg = config.${namespace}.${type}.${category}.${program};
 in
 {
+  config.nixpkgs = mkIf cfg.enable {
+    overlays = with inputs.nix-vscode-extensions; [
+      overlays.default
+    ];
+  };
+
   config.programs.${program}.profiles.${profile} = mkIf cfg.enable {
     enableExtensionUpdateCheck = mkDefault false;
-    extensions =
-      with pkgs.vscode-extensions;
-      [
-        streetsidesoftware.code-spell-checker
-        catppuccin.catppuccin-vsc-icons
-        formulahendry.auto-rename-tag
-        github.vscode-github-actions
-        bradlc.vscode-tailwindcss
-        editorconfig.editorconfig
-        yoavbls.pretty-ts-errors
-        astro-build.astro-vscode
-        tamasfe.even-better-toml
-        myriad-dreamin.tinymist
-        mechatroner.rainbow-csv
-        rust-lang.rust-analyzer
-        oderwat.indent-rainbow
-        dbaeumer.vscode-eslint
-        ritwickdey.liveserver
-        usernamehw.errorlens
-        # dotenv.dotenv-vscode # wants to write to `settings.json` so gives errors as nix is read only
-        wmaurer.change-case
-        jnoortheen.nix-ide
-        bierner.color-info
-        # mhutchie.git-graph # Unfree :/
-        dotjoshjohnson.xml
-        eamodio.gitlens
-        tomoki1207.pdf
-        docker.docker
-        antfu.slidev
-        vue.volar
-        golang.go
-        jock.svg
-      ]
-      ++ [
-        # Python and Jupyter notebooks
-        ms-toolsai.jupyter-renderers
-        ms-toolsai.jupyter
-        ms-python.debugpy
-        ms-python.pylint
-        ms-python.python
-      ];
+    extensions = with pkgs.nix-vscode-extensions.vscode-marketplace; [
+      streetsidesoftware.code-spell-checker
+      catppuccin.catppuccin-vsc-icons
+      formulahendry.auto-rename-tag
+      github.vscode-github-actions
+      bradlc.vscode-tailwindcss
+      editorconfig.editorconfig
+      yoavbls.pretty-ts-errors
+      astro-build.astro-vscode
+      tamasfe.even-better-toml
+      myriad-dreamin.tinymist
+      mechatroner.rainbow-csv
+      rust-lang.rust-analyzer
+      oderwat.indent-rainbow
+      dbaeumer.vscode-eslint
+      ritwickdey.liveserver
+      usernamehw.errorlens
+      wmaurer.change-case
+      jnoortheen.nix-ide
+      bierner.color-info
+      dotjoshjohnson.xml
+      eamodio.gitlens
+      tomoki1207.pdf
+      docker.docker
+      antfu.slidev
+      vue.volar
+      golang.go
+      jock.svg
+      # Python and Jupyter notebooks
+      ms-toolsai.jupyter-renderers
+      ms-toolsai.jupyter
+      ms-python.debugpy
+      ms-python.pylint
+      ms-python.python
+    ];
   };
 }
